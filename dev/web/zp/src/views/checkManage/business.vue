2d99c566f2d1dd331bee00fd2c7d56c7feac5d30
Leihaha937 <937189404@qq.com>
2019-12-29 14:42:10 +0800

    'haha'

--- dev/web/zp/src/views/checkManage/business.vue
+++ dev/web/zp/src/views/checkManage/business.vue
@@ -3,10 +3,11 @@
  * 商家审核页面
  * @Date: 2019-12-23 17:11:53 
  * @Last Modified by: LeiHaha
- * @Last Modified time: 2019-12-27 22:06:47
+ * @Last Modified time: 2019-12-29 11:30:28
  */
 <template>
   <div id="businessCheck">
+    <!-- {{statusList}} -->
     <div class="searchDiv">
       <el-select @change="statusChange" v-model="status" clearable placeholder="全部">
         <el-option
@@ -18,8 +19,8 @@
       </el-select>
     </div>
     <div class="tebleDiv">
-      <el-table ref="multipleTable" :data="businessData" tooltip-effect="dark" style="width: 100%" >
-        <el-table-column align="center" type="selection" width="55"></el-table-column>
+      <el-table ref="multipleTable" :data="businessList" tooltip-effect="dark" style="width: 100%" @selection-change="selectionChange">
+      <el-table-column align="center" type="selection" width="55"></el-table-column>
         <el-table-column align="center" prop="name" label="企业名称"></el-table-column>
         <el-table-column align="center" prop="contactName" label="联系人"></el-table-column>
         <el-table-column align="center" prop="contactPhone" label="联系方式"></el-table-column>
@@ -35,59 +36,161 @@
             <el-button @click="toSee(scope.row)" type="text" size="small">查看</el-button>
           </template>
         </el-table-column>
-        <el-table-column align="center" prop="status" label="操作"></el-table-column>
-        <!-- <el-table-column align="center" fixed="right" label="操作" width="100">
+          <el-table-column align="center" prop="status" label="操作" width="200">
           <template slot-scope="scope">
-            <el-button type="success" size="mini" @click="toEdit(scope.row)">通过</el-button>
-            <el-button type="danger" size="mini" @click="toDelete(scope.row.id)">拒绝</el-button>
+            <div v-if="scope.row.status === '审核通过'">
+              <div>{{scope.row.status}}</div>
+            </div>
+            <div v-if="scope.row.status === '拒绝'">
+              <div>{{scope.row.status}}</div>
+            </div>
+            <div v-if = "scope.row.status === '待审核'">
+              <el-button type="success" size="mini" @click="toEdit(scope.row)" >通过</el-button>
+              <el-button type="danger " size="mini" @click="toDelete(scope.row)">拒绝</el-button>
+            </div> 
           </template>
-        </el-table-column> -->
+        </el-table-column>
       </el-table>
     </div>
     <div class="footerDiv">
       <div class="btnDiv">
-        <el-button @click="toBatchDelete" size="mini" type="danger" plain>批量删除</el-button>
+        <el-button @click="toBatchStatus" size="mini" type="success" round>一键通过</el-button>
       </div>
       <div class="pageDiv">
         <el-pagination :page-size="pageSize" :current-page.sync="currentPage"
-          @current-change="pageChange"
             background
+          @current-change="pageChange"
             layout= "prev, pager, next"
             :total="businessData.length">
         </el-pagination>
       </div>
     </div>
-    {{businessData}}
-    {{statusData}}
-    {{staData}}
+    <!-- 查看 -->
+    <el-dialog
+      :title="currentBus.name"
+      :visible.sync="seeVisible">
+      <div class="seeDiv">
+        {{currentBus.industry}}
+      </div>
+      <div class="seeDiv">
+        {{currentBus.scale}}
+      </div>
+      <div class="seeDiv">
+        <span>成立时间:</span>
+        {{currentBus.establishedTime}}
+      </div>
+      <div class="seeDiv">
+        <span>注册资本:</span>
+        {{currentBus.registeredCapital}}
+      </div>
+      <div class="descDiv">&nbsp;&nbsp;&nbsp;&nbsp;{{currentBus.description}}</div>
+      <div class="imgDiv">
+        <a :href="currentBus.businessLicense" target="_blank">
+          <img :src="currentBus.businessLicense" alt width="200" height="150" />
+        </a>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import {findAllBusiness} from '@/api/business.js'
+import {findAllBusiness,saveOrUpdateBusiness,findBusinessById} from '@/api/business.js'
 import config from '@/utils/config.js';
 export default {
   data() {
     return {
+      status:'',
       //商家数组
       businessData:[],
       //操作数组
       statusData:[],
       //状态数组
       staData:[],
-      //商家列表
+      //商家列表 
       statusList:[],
+      //一键通过
+      ids:[],
+      //每页条数
+      pageSize:config.pageSize,
+      //当前页
+      currentPage:1,
+      //id查找出的商家数组
+      idStaus:[],
+      //查看模态框显示与否
+      seeVisible:false,
+      //当前查看或修改的对象
+      currentBus:{},
     };
   },
-  computed: {},
+  computed: {
+    //分页数据
+    businessList(){
+      let temp =[...this.businessData];
+      let page = this.currentPage;
+      let pageSize = config.pageSize;
+      return temp.slice((page-1)*pageSize,page*pageSize);
+    },
+  },
   methods: {
-    //选项发生改变
-    statusChange(val){
-      this.statusData.forEach(item =>{
-        if(item.statusData ===val ){
-          return item.statusData;
-        }
+    //查看
+    toSee(row){
+      this.currentBus = {...row};
+      this.seeVisible = true;
+    },
+    //拒绝
+    async toDelete(val){
+      val.status="拒绝";
+      // console.log(val,'----------');
+      let res = await saveOrUpdateBusiness(val);
+    },
+    //通过
+    async toEdit(val){
+      val.status="审核通过";
+      // console.log(val,'----------');
+      let res = await saveOrUpdateBusiness(val);
+    },
+    //页数发生改变
+    pagechange(page){
+      this.currentPage = page;
+    },
+    //一键通过
+    toBatchStatus(){
+      //获取要一键通过的id
+      let ids = this.ids;
+      this.ids.forEach(async(item)=>{
+        let res = await findBusinessById({id:item});
+        res.data.status="审核通过";
+        // console.log(res,'---------------------------');
+        let rs = await saveOrUpdateBusiness(res.data);
+        this.findAllBus();
+      })
+    },
+    //复选框选中切换
+    selectionChange(val){
+      //val 就是选中的对象组成数组
+      // console.log(val);
+      let ids = val.map((item)=>{
+        return item.id;
+      });
+      this.ids = ids;
+    },
 
+    //选项发生改变d
+    statusChange(val){
+      // console.log( this.businessData)
+      // console.log( val)
+      let rs = [];
+      this.businessData.forEach(item=>{
+        if(item.status === val){
+          // alert(1)
+          rs.push(item);
+        }else{
+          if(val===''){
+            this.findAllBus();
+          }
+          }
+          this.businessData=rs;      
+         console.log(rs)
       })
     },
     //查找所有商家信息
@@ -116,5 +219,34 @@ export default {
   mounted() {}
 };
 </script>
-<style scoped>
+<style lang='scss' scoped>
+  .footerDiv{
+    margin-top: 10px;
+    overflow: hidden;
+    .btnDiv{
+      float: left;
+     }
+    .pageDiv{
+      float: right;
+    }
+  }
+  .seeDiv{
+  font-weight: bolder;
+  border-bottom: 1px solid #ccc;
+  line-height:30px;
+  span{
+    color: #777;
+  }
+}
+.descDiv{
+  font-weight: bold;
+  color: #777;
+  font-size: 12px;
+  line-height: 30px;
+  padding: 10px 0;
+  border-bottom: 1px solid #ccc;
+}
+.imgDiv{
+  text-align: center;
+}
 </style>
--- dev/web/zp/src/views/checkManage/recruit.vue
+++ dev/web/zp/src/views/checkManage/recruit.vue
@@ -2,23 +2,265 @@
  * @Author: liuyr 
  * 招聘审核页面
  * @Date: 2019-12-23 17:11:53 
- * @Last Modified by: liuyr
- * @Last Modified time: 2019-12-23 21:38:39
+ * @Last Modified by: LeiHaha
+ * @Last Modified time: 2019-12-29 13:56:09
  */
 <template>
-  <div id="recruitCheck">招聘审核页面</div>
+  <div id="doingData">
+    <div class="searchDiv">
+      <el-select @change="jobChange" size="mini" v-model="job" clearable placeholder="职位类型">
+        <el-option
+          v-for="item in jobData"
+          :key="item"
+          :label="item"
+          :value="item">
+        </el-option>
+      </el-select>
+    </div>
+    <div class="tebleDiv">
+      <el-table ref="multipleTable" :data="doingList" tooltip-effect="dark" style="width: 100%" @selection-change="selectionChange">
+        <el-table-column align="center" type="selection" width="55"></el-table-column>
+        <el-table-column align="center" prop="title" label="招聘标题"></el-table-column>
+        <el-table-column align="center" prop="contactName" label="发布人"></el-table-column>
+        <el-table-column align="center" prop="contactPhone" label="联系方式"></el-table-column>
+        <el-table-column align="center" prop="job" label="职位"></el-table-column>
+        <el-table-column align="center" prop="publishTime" label="发布时间"></el-table-column>
+        <el-table-column align="center" label="详情">
+          <template slot-scope="scope">
+            <el-button @click="toSee(scope.row)" type="text" size="small">查看</el-button>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" fixed="right" label="操作" width="200">
+          <template slot-scope="scope">
+            <div v-if="scope.row.status === '审核通过'">
+              <div>{{scope.row.status}}</div>
+            </div>
+            <div v-if="scope.row.status === '拒绝'">
+              <div>{{scope.row.status}}</div>
+            </div>
+            <div v-if="scope.row.status === '招聘中'">
+              <div>{{scope.row.status}}</div>
+            </div>
+            <div v-if="scope.row.status === '招聘完成'">
+              <div>{{scope.row.status}}</div>
+            </div>
+            <div v-if = "scope.row.status === '待审核'">
+              <el-button type="success" size="mini" @click="toEdit(scope.row)" >通过</el-button>
+              <el-button type="danger " size="mini" @click="toDelete(scope.row)">拒绝</el-button>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="footerDiv">
+      <div class="btnDiv">
+        <el-button @click="toBatchStatus" size="mini" type="success" round>一键通过</el-button>
+      </div>
+      <div class="pageDiv">
+        <el-pagination :page-size = "pageSize" :current-page.sync = "currentPage"
+          @current-change = "pageChange"
+          background
+          layout= "prev, pager, next"
+          :total="doingData.length">
+        </el-pagination>
+      </div>
+    </div>
+    <!-- 查看 -->
+    <el-dialog
+      :title="currentBus.title"
+      :visible.sync="seeVisible">
+      <div class="seeDiv">
+        {{currentBus.job}}
+      </div>
+      <div class="seeDiv">
+        {{currentBus.scale}}
+      </div>
+      <div class="seeDiv">
+        <span>发布时间:</span>
+        {{currentBus.publishTime}}
+      </div>
+      <div class="seeDiv">
+        <span>薪资:</span>
+        {{currentBus.salary}}
+      </div>
+      <div class="descDiv">&nbsp;&nbsp;&nbsp;&nbsp;{{currentBus.welfare}}</div>
+      <div class="imgDiv">
+        <a :href="currentBus.businessLicense" target="_blank">
+          <img :src="currentBus.businessLicense" alt width="200" height="150" />
+        </a>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
+import { findAllEmployment,findEmploymentByJob,saveEmploymentOrUpdate,findEmploymentById} from "@/api/employment.js";
+import config from '@/utils/config.js';
 export default {
   data() {
-    return {};
+    return {
+      //职位
+      job:'',
+      //职位数组
+      jobData:[],
+      //招聘中信息数组
+      doingData:[],
+      //招聘中信息列表
+      // doingList:[],
+      //一键通过ids
+      ids:[],
+      //每页条数
+      pageSize:config.pageSize,
+      //当前页
+      currentPage:1,
+      //查看模态框显示与否
+      seeVisible:false,
+      //当前查看的对象
+      currentBus:{},
+    };
   },
-  computed: {},
-  methods: {},
-  created() {},
+  computed: {
+    //分页数据
+    doingList(){
+      let temp =[...this.doingData];
+      let page = this.currentPage;
+      let pageSize = config.pageSize;
+      return temp.slice((page-1)*pageSize,page*pageSize);
+    },
+  },
+  methods: {
+    //页数发生改变
+    pagechange(page){
+      this.currentPage = page;
+    },
+  //复选框选中切换
+  selectionChange(val){
+    //val 就是选中的对象组成数组
+    // console.log(val);
+    let ids = val.map((item)=>{
+      return item.id;
+    });
+    this.ids = ids;
+  },
+    //职位类型发生改变
+    async jobChange(val){
+      if(val){
+        try {
+          let res = await findEmploymentByJob({job:val});
+          this.doingData = res.data;
+        } catch (error) {
+          config.errorMsg(this,'通过职位类型查找招聘信息错误');
+        }
+      }else{
+        this.findAllEmp();
+      }
+    },
+    //查看
+    toSee(row){
+      this.currentBus = {...row};
+      this.seeVisible = true;
+    },
+    //通过
+    async toEdit(val){
+      console.log(val,"=================")
+      val.status="审核通过";
+      console.log(val,'----------');
+      try {
+        let res = await saveEmploymentOrUpdate(val);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    //一键通过
+    toBatchStatus(){
+      //获取要一键通过的id
+      let ids = this.ids;
+      this.ids.forEach(async(item)=>{
+        let res = await findEmploymentById({id:item});
+        res.data.status="审核通过";
+        console.log(res,'---------------------------');
+        let rs = await saveEmploymentOrUpdate(res.data);
+        // this.findAllBus();
+      })
+    },
+    //拒绝
+    async toDelete(val){
+      val.status="拒绝";
+      // console.log(val,'----------');
+      let res = await saveEmploymentOrUpdate(val);
+    },
+    //时间数据清洗
+    timeDataClear(){
+      this.doingData.forEach(item=>{
+        item.publishTime=item.publishTime.slice(0,10);
+      })
+    },
+    //查找所有的招聘信息
+    async findAllEmp(){
+      try {
+        let res = await findAllEmployment();
+        this.doingData = res.data;
+        // console.log(this.doingData);
+        // this.doingData.publishTime = '';
+        // //时间数组
+        // let timeArr = res.data.map((item)=>{
+        //   return item.publishTime.slice(0,10);
+        // })
+        // this.publishTime = timeArr;
+        this.timeDataClear();
+        // console.log(this.doingData);
+        // item.publishTime = timeArr;
+        //职位数组 
+        let jobArr = res.data.map((item)=>{
+          return item.job;
+        });
+        this.jobData = [...new Set(jobArr)];
+      } catch (error) {
+        config.errorMsg(this,'查找错误');
+      }
+    },
+  },
+  created() {
+    this.findAllEmp();
+    
+  },
   mounted() {}
 };
 </script>
-<style scoped>
+<style lang="scss" scoped>
+.searchDiv{
+  margin-top: 10px;
+}
+.tebleDiv{
+  margin-top: 10px;
+}
+.footerDiv{
+  margin-top: 10px;
+  overflow: hidden;
+  .btnDiv{
+    float: left;
+  } 
+  .pageDiv{
+    float: right;
+  }
+}
+.seeDiv{
+  font-weight: bolder;
+  border-bottom: 1px solid #ccc;
+  line-height:30px;
+  span{
+    color: #777;
+  }
+}
+.descDiv{
+  font-weight: bold;
+  color: #777;
+  font-size: 12px;
+  line-height: 30px;
+  padding: 10px 0;
+  border-bottom: 1px solid #ccc;
+}
+.imgDiv{
+  text-align: center;
+}
 </style>
